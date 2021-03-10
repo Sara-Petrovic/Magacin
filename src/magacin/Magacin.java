960f71def526bbd928b2df1c24cafe4a3ff5ade0
Sara-Petrovic <sarapetrovicue@gmail.com>
2021-03-10 14:56:54 +0100

    Kreirana klasa Magacin koja implementira interfejs MagacinInterfejs

---
+++ src/magacin/Magacin.java
@@ -0,0 +1,26 @@
+package magacin;
+
+import artikli.Artikal;
+import magacin.interfejs.MagacinInterfejs;
+
+public class Magacin implements MagacinInterfejs {
+
+	@Override
+	public void dodajArtikal(Artikal artikal) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void izbaciArtikal(Artikal artikal) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Artikal pronadjiArtikal(int sifra) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
