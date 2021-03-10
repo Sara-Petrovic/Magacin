455a76cc67e9573dde15f06dace6aac22c29ac1e
Sara-Petrovic <sarapetrovicue@gmail.com>
2021-03-10 15:09:50 +0100

    Implementirane metode klase Magacin na master grani

--- src/magacin/Magacin.java
+++ src/magacin/Magacin.java
@@ -1,25 +1,48 @@
 package magacin;
 
+import java.util.LinkedList;
+
 import artikli.Artikal;
 import magacin.interfejs.MagacinInterfejs;
 
 public class Magacin implements MagacinInterfejs {
 
+	private LinkedList<Artikal> artikli=new LinkedList<Artikal>();
+	
 	@Override
 	public void dodajArtikal(Artikal artikal) {
-		// TODO Auto-generated method stub
+		if(artikal!=null && !artikli.contains(artikal)) {
+			artikli.add(artikal);
+		}
+		else if(artikal!=null) {
+			for(Artikal a:artikli) {
+				if(a.equals(artikal)) {
+					a.setKolicina(a.getKolicina()+artikal.getKolicina());
+				}
+			}
+		}
 
 	}
 
 	@Override
 	public void izbaciArtikal(Artikal artikal) {
-		// TODO Auto-generated method stub
+		if(artikal!=null) {
+			for(Artikal a:artikli) {
+				if(a.equals(artikal)) {
+					a.setKolicina(a.getKolicina()-artikal.getKolicina());
+				}
+			}
+		}
 
 	}
 
 	@Override
 	public Artikal pronadjiArtikal(int sifra) {
-		// TODO Auto-generated method stub
+		for(Artikal a:artikli) {
+			if(a.getSifra()==sifra) {
+				return a;
+			}
+		}
 		return null;
 	}
 
