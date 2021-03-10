25d793bdce0a3441ed51fa11fe522f1d8398066b
Sara-Petrovic <sarapetrovicue@gmail.com>
2021-03-10 14:15:37 +0100

    Kreiran interfejs MagacinInterfejs

---
+++ src/magacin/interfejs/MagacinInterfejs.java
@@ -0,0 +1,13 @@
+package magacin.interfejs;
+
+import artikli.Artikal;
+
+public interface MagacinInterfejs {
+	
+	public void dodajArtikal(Artikal artikal);
+	
+	public void izbaciArtikal(Artikal artikal);
+	
+	public Artikal pronadjiArtikal(int sifra);
+	
+}
